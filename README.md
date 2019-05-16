ab0bb712cb20735ffa4a497ad8c557c2e53904f2
Sergey Stolyarov <sergei@regolit.com>
2019-05-16 15:39:29 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+SmartCard Reader
+================
+
+Scans smartcard and displays found data.
+
+Written in Java/JavaFX. 
+
+Requirements: JDK 11+, JavaFX 11+, pcscd (linux only).
---
+++ src/main/java/com/regolit/jscreader/Main.java
@@ -0,0 +1,9 @@
+/* INSERT LICENSE HERE */
+
+package com.regolit.jscreader;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("MAIN CLASS");
+	}
+}
