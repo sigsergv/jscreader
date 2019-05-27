7f34faa3b94cc2e4fe8366b94530ad5dc81b663b
Sergey Stolyarov <sergei@regolit.com>
2019-05-28 00:21:41 +0700

    Updated script

--- tools/download-aid-list.py
+++ tools/download-aid-list.py
@@ -3,13 +3,14 @@
 import urllib.request
 import tempfile
 import re
+import codecs
 from sys import exit
 
 SOURCE = 'https://www.eftlab.com/index.php/site-map/knowledge-base/211-emv-aid-rid-pix'
-# r = urllib.request.urlopen(SOURCE)
-# html = r.read()
+r = urllib.request.urlopen(SOURCE)
+html = r.read()
 
-html = open('/tmp/test.html').read()
+# html = codecs.open('/tmp/test.html', encoding='utf8').read()
 
 ROWS_RE = re.compile('<tr.*?>(.+?)</tr>', re.MULTILINE+re.DOTALL)
 ROW_RE = re.compile('<td.*?>(.+?)</td>.*?<td.*?>(.+?)</td>.*?<td.*?>(.+?)</td>.*?<td.*?>(.+?)</td>.*?<td.*?>(.+?)</td>.*?<td.*?>(.+?)</td>.*?', re.MULTILINE+re.DOTALL)
@@ -20,15 +21,21 @@ for row in ROWS_RE.findall(html):
         # print('Invalid row: %s' % row)
         continue
     i += 1
-    aid = mo.group(1)
+    aid, vendor, country, name, description, t = mo.groups()
+
     # reformat AID
     if len(aid) % 2 != 0:
         print('INVALID AID LENGTH')
         exit(123)
     paid = ' '.join( [x+y for (x,y) in zip(aid[0::2], aid[1::2])] )
     
-
-    print('<application aid="{aid}" type="" name=""/>'.format(aid=paid))
+    if description == '&nbsp;':
+        description = ''
+    if name == '&nbsp;':
+        name = ''
+    if t == '&nbsp;':
+        t = ''
+    print('<application aid="{aid}" type="{type}" name="{name}" description="{description}" enabled="false"/>'.format(aid=paid, description=description, name=name, type=t))
     # print(mo.groups())
 
 print(i)
